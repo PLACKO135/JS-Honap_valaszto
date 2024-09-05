9e31beecf0f589c92b82052c26b61018be2a798e
Papp László Levente <papp.laszlo.levente@burg.lo.hu>
2024-09-05 09:53:30 +0200

    safety save

--- script.js
+++ script.js
@@ -3,35 +3,45 @@
 let button = document.querySelector('input[type="button"]');
 let calendar_div = document.getElementsByClassName("calendar");
 let honapok_select = document.getElementById("Honapok_select");
-let honap = honapok_select.value;
+const honap = honapok_select.value;
 
-const monthLengthsArray = [
-  31, // January
-  28, // February
-  31, // March
-  30, // April
-  31, // May
-  30, // June
-  31, // July
-  31, // August
-  30, // September
-  31, // October
-  30, // November
-  31 // December
-];
+select.addEventListener('change', createTable);
 
-
-button.addEventListener('click', createTable);
+const monthLengths = {
+  January: 31,
+  February: 28,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31
+};
 
 function createTable() {
+    const rowAmount = monthLengths[honapok_select.value];
 
-  let calendarDiv = document.querySelector('.calendar');
+  const table = document.createElement('table');
 
-  let table = document.createElement('table');
-  for (let index = 0; index < array.length; index++) {
-    let tr = document.createElement('tr');    
-    
+  for (let i = 0; i < rowAmount; i++) {
+    const row = document.createElement('tr');
+    table.appendChild(row);
+
+    for (let j = 0; j < 7; j++) {
+      const cell = document.createElement('td');
+      row.appendChild(cell);
+    }
   }
-  calendarDiv.appendChild(table);
-
+  calendar_div.appendChild(table);
 }
+
+
+
+
+
+
+createTable();
